5cac99ba93f4d771a0b895132945d0a625049274
Teddy Yongo <teyddie@gmail.com>
2023-11-07 20:24:08 +0300

    fixes for the navigation and change of overall look

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	mode: "jit",
 	purge: ["./src/index.html"],
 	content: ["./src/**/*.{html,js}"],
-	darkMode: false, // or 'media' or 'class'
+	media: false, // or 'media' or 'class'
 	theme: {},
 	variants: {},
 	plugins: [],
